240537776fac1e0d694a3f5202235b6057a52e65
Chloé Gerry <cgerry@corp.free.fr>
2024-02-28 14:43:20 +0100

    add algorith condition

--- scripts/utils/searchRecipe-forLoop.js
+++ scripts/utils/searchRecipe-forLoop.js
@@ -4,30 +4,45 @@ export const searchRecipeForLoop = (recipes) => {
   const searchInput = document.getElementsByClassName("search-input")[0];
   searchInput.addEventListener("input", (event) => {
     event.preventDefault();
-    const inputValue = event.target.value;
+    let inputValue = event.target.value;
     const matchingRecipes = [];
 
-    for (let i = 0; i < recipes.length; i++) {
-      for (let j = 0; j < recipes[i].ingredients.length; j++) {
-        if (
-          recipes[i].name.toLowerCase().match(inputValue.toLowerCase()) ||
-          recipes[i].description.toLowerCase().match(inputValue.toLowerCase()) ||
-          recipes[i].ingredients[j].ingredient.toLowerCase().match(inputValue.toLowerCase())
-        ) {
-          if (!matchingRecipes.includes(recipes[i])) {
-            matchingRecipes.push(recipes[i]);
+    if (inputValue.length > 2) {
+      // const recipesWrapper = document.getElementsByClassName("recipes-wrapper")[0];
+      // const recipeTemplate = new RecipeTemplateClass();
+      // let displayMatchingRecipes = "";
+
+      const deleteSearchIcon = document.getElementsByClassName("search-input-reset-icon")[0];
+      deleteSearchIcon.style.visibility = "visible";
+
+      deleteSearchIcon.addEventListener("click", () => {
+        event.target.value = "";
+        // recipes.map((recipe) => (displayMatchingRecipes += recipeTemplate.getRecipeCard(recipe)));
+        // recipesWrapper.innerHTML = displayMatchingRecipes;
+      });
+
+      for (let i = 0; i < recipes.length; i++) {
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+          if (
+            recipes[i].name.toLowerCase().match(inputValue.toLowerCase()) ||
+            recipes[i].description.toLowerCase().match(inputValue.toLowerCase()) ||
+            recipes[i].ingredients[j].ingredient.toLowerCase().match(inputValue.toLowerCase())
+          ) {
+            if (!matchingRecipes.includes(recipes[i])) {
+              matchingRecipes.push(recipes[i]);
+            }
           }
+
+          const recipesWrapper = document.getElementsByClassName("recipes-wrapper")[0];
+          const recipeTemplate = new RecipeTemplateClass();
+          let displayMatchingRecipes = "";
+
+          matchingRecipes.map(
+            (recipe) => (displayMatchingRecipes += recipeTemplate.getRecipeCard(recipe))
+          );
+          recipesWrapper.innerHTML = displayMatchingRecipes;
         }
-
-        const recipesWrapper = document.getElementsByClassName("recipes-wrapper")[0];
-        const recipeTemplate = new RecipeTemplateClass();
-        let displayMatchingRecipes = "";
-
-        matchingRecipes.map(
-          (recipe) => (displayMatchingRecipes += recipeTemplate.getRecipeCard(recipe))
-        );
-        recipesWrapper.innerHTML = displayMatchingRecipes;
       }
-    }
+    } else return;
   });
 };
